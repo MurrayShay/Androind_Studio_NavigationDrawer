38d21b1bfedb76e204dcad61e145425a1073c138
MurrayShay <komlllcf@gmail.com>
2015-10-23 01:33:05 +0800

    v1.1

--- app/src/main/java/demo/wp/environmentapp_1/MainActivity.java
+++ app/src/main/java/demo/wp/environmentapp_1/MainActivity.java
@@ -23,11 +23,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         navigationView = (NavigationView) findViewById(R.id.navigation_view);
         navigationView.setNavigationItemSelectedListener(this);
+        //註冊 drawer 打開關閉事件
+        //ActionBarDrawerToggle 已經實做 DrawerLayout.DrawerListener 介面
         actionBarDrawerToggle = new ActionBarDrawerToggle(this,
                 drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer) {
             public void onDrawerClosed(View drawerView) {
@@ -78,11 +81,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 
+    // onPostCreate方法是指onCreate方法彻底执行完毕的回调，onPostResume类似，
+    // 这两个方法官方说法是一般不会重写，现在知道的做法也就只有在使用ActionBarDrawerToggle的使用
+    // 在onPostCreate需要在屏幕旋转时候等同步下状态，Google官方提供的一些实例就是
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         actionBarDrawerToggle.syncState();
     }
 
+    // onContentChanged()是Activity中的一个回调方法 当Activity的布局改动时，
+    // 即setContentView()或者addContentView()方法执行完毕时就会调用该方法，
+    // 例如，Activity中各种View的findViewById()方法都可以放到该方法中。
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         actionBarDrawerToggle.onConfigurationChanged(newConfig);
